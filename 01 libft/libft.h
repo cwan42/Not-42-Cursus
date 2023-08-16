4ea705c2d01baefd11af2f51801c1309b903d86f
cwan42 <136786922+cwan42@users.noreply.github.com>
2023-08-16 15:45:32 +0800

    Add files via upload

--- 01 libft/libft.h
+++ 01 libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 14:25:45 by cwan              #+#    #+#             */
-/*   Updated: 2023/08/14 16:11:57 by cwan             ###   ########.fr       */
+/*   Updated: 2023/08/16 15:44:35 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,17 @@ int	ft_isascii(int c);
 int	ft_isprint(int c);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
+int	ft_atoi(const char *str);
+
+int	ft_strlen(char *str);
 
 void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl(char *s);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr(int n);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr(char *str);
+void	ft_putstr_fd(char *str, int fd);
 
 #endif
