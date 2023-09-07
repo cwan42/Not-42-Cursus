e5029a2d0ab4ce55ed631778dec02342f5242205
Christopher Wan <cwan@c1r7s9.42singapore.sg>
2023-09-07 14:45:48 +0800

    Updated libft.h

--- 01 libft/libft.h
+++ 01 libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 14:25:45 by cwan              #+#    #+#             */
-/*   Updated: 2023/09/07 10:32:49 by cwan             ###   ########.fr       */
+/*   Updated: 2023/09/07 14:37:18 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,11 @@ int	ft_atoi(const char *str);
 
 int	ft_strlen(char *str);
 
-void	ft_putchar(char c);
 void	ft_putchar_fd(char c, int fd);
-void	ft_putendl(char *s);
 void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr(int n);
 void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr(char *str);
 void	ft_putstr_fd(char *str, int fd);
 
-void	*ft_memset(void *b, int c, size_t len);
+//void	*ft_memset(void *b, int c, size_t len);
 
 #endif
