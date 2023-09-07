e8e08c0605a9b8509f6004e25a7c45d47ef621b7
Christopher Wan <cwan@c1r7s9.42singapore.sg>
2023-09-07 15:25:05 +0800

    Added bzero, memset

--- 01 libft/ft_bzero.c
+++ 01 libft/ft_bzero.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/07 08:59:20 by cwan              #+#    #+#             */
-/*   Updated: 2023/09/07 14:51:12 by cwan             ###   ########.fr       */
+/*   Created: 2023/09/07 14:46:37 by cwan              #+#    #+#             */
+/*   Updated: 2023/09/07 14:47:22 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	ft_bzero(void *str, size_t n)
 {
-	size_t			i;
-	unsigned char	*bc;
-	unsigned char	cc;
-
-	i = 0;
-	bc = (unsigned char *)b;
-	cc = (unsigned char)c;
-	while (i < len)
-		bc[i++] = cc;
-	return (b);
+	ft_memset(str, '\0', n);
 }
--- 01 libft/libft.h
+++ 01 libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 14:25:45 by cwan              #+#    #+#             */
-/*   Updated: 2023/09/07 14:37:18 by cwan             ###   ########.fr       */
+/*   Updated: 2023/09/07 14:52:22 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,23 @@
 
 # include <unistd.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_atoi(const char *str);
 
-int	ft_strlen(char *str);
+int		ft_strlen(char *str);
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *str, int fd);
 
-//void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *str, size_t n);
 
 #endif
