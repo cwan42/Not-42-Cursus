122c54cb6d7c170e0cde91e3fc501f43fa544f76
Christopher Wan <cwan@c1r7s9.42singapore.sg>
2023-09-11 17:02:26 +0800

    Added WIP substr

---
+++ 01 libft/ft_substr.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 15:39:18 by cwan              #+#    #+#             */
+/*   Updated: 2023/09/11 17:02:07 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	slen;
+
+	slen = ft_strlen(s);
+	if (start >= s_len)
+		return (NULL);
+	
+}
