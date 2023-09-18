b194217115ed6966c9f9787519f6ea964d14401e
Christopher Wan <cwan@c1r7s9.42singapore.sg>
2023-09-18 20:46:49 +0800

    Refactored memchr, memcmp, strncmp, strnstr

--- 01 libft/ft_memchr.c
+++ 01 libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:43:54 by cwan              #+#    #+#             */
-/*   Updated: 2023/09/11 14:32:06 by cwan             ###   ########.fr       */
+/*   Updated: 2023/09/18 20:37:08 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*src;
-	unsigned char		cha;
+	const unsigned char	*str;
 	size_t				i;
 
-	src = (const unsigned char *)s;
-	cha = (unsigned char)c;
+	str = (const unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
-		if (src[i] == cha)
-			return ((void *)(src + i));
+		if (str[i] == (unsigned char)c)
+			return ((void *)(str + i));
 		i++;
 	}
 	return (NULL);
