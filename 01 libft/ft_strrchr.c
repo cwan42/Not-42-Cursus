9468abb59b9e550f72daa5bc5a34f3bce0a90bf9
Christopher Wan <cwan@c1r7s9.42singapore.sg>
2023-09-18 20:12:43 +0800

    Refactored ft_strrchr and fr_strtrim

--- 01 libft/ft_strrchr.c
+++ 01 libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 09:39:28 by cwan              #+#    #+#             */
-/*   Updated: 2023/09/15 14:53:27 by cwan             ###   ########.fr       */
+/*   Updated: 2023/09/18 20:06:07 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strrchr(const char *s, int c)
 
 	i = (int)ft_strlen(s) +1;
 	while (i--)
-		if (*(s + i) == (char)c)
+		if (*(s + i) == (char)c || !c)
 			return ((char *)s + i);
 	return (NULL);
 }
--- 01 libft/ft_strtrim.c
+++ 01 libft/ft_strtrim.c
@@ -6,49 +6,22 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 10:06:40 by cwan              #+#    #+#             */
-/*   Updated: 2023/09/13 11:22:03 by cwan             ###   ########.fr       */
+/*   Updated: 2023/09/18 20:00:35 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isset(char c, char const *arr)
-{
-	int	i;
-	int	len;
-
-	i = 0;
-	len = ft_strlen(arr);
-	while (i < len)
-	{
-		if (arr[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*str;
 	size_t	i;
-	size_t	start;
-	size_t	end;
 
 	if (!s1 || !set)
-		return (0);
-	i = 0;
-	start = 0;
-	end = ft_strlen(s1);
-	while (s1[start] && ft_isset(s1[start], set))
-		start++;
-	while (end > start && ft_isset(s1[end - 1], set))
-		end--;
-	str = malloc(end - start + 1);
-	if (!str)
 		return (NULL);
-	while (start < end)
-		str[i++] = s1[start++];
-	str[i] = '\0';
-	return (str);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strrchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i +1));
 }
