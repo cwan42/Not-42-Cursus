4225b1ea3c4dbb9a7bbf98e9c2b7c496f6b51ed0
Christopher Wan <cwan@c2r6s8.42singapore.sg>
2023-09-27 14:21:36 +0800

    Updated WIP gnl c and h

--- 03_gnl/get_next_line.h
+++ 03_gnl/get_next_line.h
@@ -6,15 +6,26 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 17:46:27 by cwan              #+#    #+#             */
-/*   Updated: 2023/09/26 17:50:03 by cwan             ###   ########.fr       */
+/*   Updated: 2023/09/27 10:35:32 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 69
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+typedef struct s_list
+{
+	char			*strbuffer;
+	struct s_list	*next;
+}					t_list;
 
 char	*get_next_line(int fd);
 
