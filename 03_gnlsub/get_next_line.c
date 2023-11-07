bf3e6fac6adf1f1903e262d4abef609343e0cec3
cwan <cwan@c2r6s8.42singapore.sg>
2023-11-07 15:21:58 +0800

    Added WIP files for gnlsub

---
+++ 03_gnlsub/get_next_line.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 12:49:11 by cwan              #+#    #+#             */
+/*   Updated: 2023/11/07 15:20:30 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[1024];
+	char		*theline;
+	int			bytesread;
+
+	buffer[fd] = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (buffer[fd] == NULL)
+		return (NULL);
+	bytesread = read(fd, buffer[fd], BUFFER_SIZE);
+	if (bytesread < 1)
+		return (NULL);
+	theline = buffer[fd];
+	return (theline);
+}
+/*
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(int ac, char *av[])
+{
+	int		fd;
+	char	*line;
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		while ((line = get_next_line(fd)) != NULL)
+			printf("%s", line);
+	}
+	else if (ac == 1)
+	{
+		while ((line = get_next_line(STDIN_FILENO)) != NULL)
+			printf("%s", line);
+	}
+}
+*/
--- 03_gnlsub/get_next_line_utils.c
+++ 03_gnlsub/get_next_line_utils.c
@@ -1,27 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 12:17:53 by cwan              #+#    #+#             */
-/*   Updated: 2023/11/07 15:13:07 by cwan             ###   ########.fr       */
+/*   Created: 2023/11/07 14:11:47 by cwan              #+#    #+#             */
+/*   Updated: 2023/11/07 15:21:02 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "get_next_line.h"
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10000
-# endif
+void	*ft_calloc(size_t numelem, size_t size)
+{
+	void			*array;
+	size_t			i;
+	unsigned char	*ptr;
 
-# include <stdlib.h>
-# include <unistd.h>
-
-void	*ft_calloc(size_t numelem, size_t size);
-
-char	*get_next_line(int fd);
-
-#endif
+	array = malloc(numelem * size);
+	if (!array)
+		return (NULL);
+	i = 0;
+	ptr = array;
+	while (i < (numelem * size))
+		ptr[i++] = '\0';
+	return (array);
+}
