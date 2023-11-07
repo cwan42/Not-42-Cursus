5bd1122f8ae27f6e47bdfb5b05f938b3b75cc675
cwan <cwan@c2r6s8.42singapore.sg>
2023-11-07 12:21:11 +0800

    Created 03_gnlsub folder w gnl.h

---
+++ 03_gnlsub/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 12:17:53 by cwan              #+#    #+#             */
+/*   Updated: 2023/11/07 12:20:37 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE=8
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
