ba7eea4536036119f18319abed0cda6cb8e7b91a
cwan42 <vboxuser@UbuntuOS.myguest.virtualbox.org>
2023-11-14 05:50:13 +0800

    Added working dir gnl2

---
+++ 03_gnl2/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 12:17:53 by cwan              #+#    #+#             */
+/*   Updated: 2023/11/14 05:49:49 by cwan42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+void	*ft_strcpy(char *s1, char *s2);
+void	*ft_calloc(size_t numelem, size_t size);
+
+char	*get_next_line(int fd);
+
+#endif
---
+++ 03_gnl2/get_next_line_utils.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 14:11:47 by cwan              #+#    #+#             */
+/*   Updated: 2023/11/14 05:49:15 by cwan42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s && (*s != (char)c))
+		s++;
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	int		i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	result = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		result[i++] = *s1++;
+	while (*s2)
+		result[i++] = *s2++;
+	return (result);
+}
+
+void	*ft_strcpy(char *s1, char *s2)
+{
+	while (*s2)
+		*s1++ = *s2++;
+	*s1 = '\0';
+}
+
+void	*ft_calloc(size_t numelem, size_t size)
+{
+	void			*array;
+	size_t			i;
+
+	array = malloc(numelem * size);
+	if (!array)
+		return (NULL);
+	i = 0;
+	while (i < numelem * size)
+		((char *)array)[i++] = 0;
+	return (array);
+}
