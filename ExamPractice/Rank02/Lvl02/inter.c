e4a356f1e71d36dac533a853fc924ec048aa13bb
cwan <cwan@c2r6s4.42singapore.sg>
2023-11-24 20:33:52 +0800

    Added inter Wip

---
+++ ExamPractice/Rank02/Lvl02/inter.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 17:06:36 by cwan              #+#    #+#             */
+/*   Updated: 2023/11/24 17:57:24 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strchr(char *str, char c)
+{
+	while (*str)
+	{
+		if (*str == c)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+int	main(int ac, char *av[])
+{
+	int	i;
+	int	j;
+
+loop av1 into av2
+	check if first instance of av1
+		write if first else no
+
+}
+
+/*
+Assignment name  : inter
+Expected files   : inter.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in both strings, in the order they appear in the first
+one.
+The display will be followed by a \n.
+If the number of arguments is not 2, the program displays \n.
+Examples:
+$>./inter "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+padinto$
+$>./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+df6ewg4$
+$>./inter "rien" "cette phrase ne cache rien" | cat -e
+rien$
+$>./inter | cat -e
+$
+*/
