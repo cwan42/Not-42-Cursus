94691e4f1ff3c6c1c8c8188a6b16cddd931c63f4
cwan <cwan@c2r6s8.42singapore.sg>
2023-11-27 13:24:06 +0800

    Changed printf folder to ft_printf

--- 04_pipex/pipex.h
+++ 04_pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 10:56:43 by cwan              #+#    #+#             */
-/*   Updated: 2023/11/27 13:19:21 by cwan             ###   ########.fr       */
+/*   Updated: 2023/11/27 13:23:18 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 # define PIPEX_H
 
 # include "./libft/libft.h"
-# include "./printf/ft_printf.h"
+# include "./ft_printf/ft_printf.h"
 
 #endif
