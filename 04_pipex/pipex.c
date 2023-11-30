9caaf4aab332d41b8e64ea0cca6a41b4bcfb61c2
cwan <cwan@c2r6s8.42singapore.sg>
2023-11-30 18:27:18 +0800

    Updated pipex.c

--- 04_pipex/pipex.c
+++ 04_pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 10:57:07 by cwan              #+#    #+#             */
-/*   Updated: 2023/11/30 16:17:27 by cwan             ###   ########.fr       */
+/*   Updated: 2023/11/30 18:26:47 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,25 @@
 #include <sys/wait.h>
 
 int	main(int ac, char *av[])
+//, char *envp[])
 {
+	int	pid;
+	int	pipefd[2];
+
 	if (ac != 5 || !av[1])
+		return(errno = EINVAL, perror("Wrong argument count"), 0);
+	pipe(pipefd);
+	pid = fork();
+	if (pid < 0)
+		return (errno = EPERM, perror("Failed fork: "), 0);
+	if (pid == 0)
 	{
-		errno = EINVAL;
-		return(perror("Wrong num of arguments"), 0);
-	}
-	int id = fork();
-	int n;
-	if (id == 0)
-	{
-		n = 1;
+		close(pipefd[0]);
+		printf("Child process is up\n");
 	}
 	else
 	{
-		n = 6;
+		close(pipefd[1]);
+		printf("Parent process is up\n");
 	}
-	if (id != 0)
-		wait(0);
-	int i = n;
-	while (i < n + 5)
-	{
-		printf("%d ", i);
-		fflush(stdout);
-		i++;
-	}
-	if (id != 0)
-		printf("\n");
-	return (0);
 }
