bb0fb601e0b2abd00a11a541d4dc83c21c0d1349
cwan <cwan@c2r6s8.42singapore.sg>
2023-12-04 15:04:45 +0800

    Added Lvl03 folder

---
+++ ExamPractice/Rank02/Lvl03/add_prime_sum.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_prime_sum.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 14:56:35 by cwan              #+#    #+#             */
+/*   Updated: 2023/12/04 14:57:36 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	main(int ac, char *av[])
+{
+}
+
+/*
+Assignment name  : add_prime_sum
+Expected files   : add_prime_sum.c
+Allowed functions: write, exit
+--------------------------------------------------------------------------------
+Write a program that takes a positive integer as argument and displays the sum
+of all prime numbers inferior or equal to it followed by a newline.
+If the number of arguments is not 1, or the argument is not a positive number,
+just display 0 followed by a newline.
+Yes, the examples are right.
+Examples:
+$>./add_prime_sum 5
+10
+$>./add_prime_sum 7 | cat -e
+17$
+$>./add_prime_sum | cat -e
+0$
+$> */
