617795eb78b17659c4b8c5ade28d861eca2b0bc9
cwan <cwan@c2r6s8.42singapore.sg>
2023-12-14 17:02:54 +0800

    Updated add_prime_sum wip

--- ExamPractice/Rank02/Lvl03/add_prime_sum.c
+++ ExamPractice/Rank02/Lvl03/add_prime_sum.c
@@ -6,14 +6,45 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 14:56:35 by cwan              #+#    #+#             */
-/*   Updated: 2023/12/04 14:57:36 by cwan             ###   ########.fr       */
+/*   Updated: 2023/12/14 16:59:17 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+//int	ft_putnbr()
+
+//int	ft_isprime(int num)
+
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	long	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] <= 32)
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	while (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
+	}
+	return (sign * res);
+}
+
+#include <stdio.h>
+
 int	main(int ac, char *av[])
 {
+	if (ac == 2)
+		printf("%d", ft_atoi(av[1]));
 }
 
 /*
