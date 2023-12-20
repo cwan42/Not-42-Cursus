9b065102aaa9c01aa0a93bf73a02d863a32559f4
cwan <cwan@c2r6s8.42singapore.sg>
2023-12-20 11:44:14 +0800

    added range, rrange and hidenp

---
+++ ExamPractice/Rank02/Lvl03/ft_rrange.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rrange.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 10:52:30 by cwan              #+#    #+#             */
+/*   Updated: 2023/12/20 11:05:22 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_abs(int start, int end)
+{
+	int	i;
+
+	i = 0;
+	if (start <= end)
+	{
+		while (start <= end)
+		{
+			i++;
+			start++;
+		}
+	}
+	else
+	{
+		while (start >= end)
+		{
+			i++;
+			start--;
+		}
+	}
+	return (i);
+}
+
+int	*ft_rrange(int start, int end)
+{
+	int	i;
+	int	*arr;
+
+	i = 0;
+	arr = malloc(sizeof(int) * ft_abs(start, end));
+	if (end >= start)
+	{
+		while (end >= start)
+			arr[i++] = end--;
+	}
+	else if (end <= start)
+	{
+		while (end <= start)
+			arr[i++] = end++;
+	}
+	return (arr);
+}
+/*
+#include <stdio.h>
+
+int	main(int ac, char *av[])
+{
+	if (ac != 3)
+		return (1);
+	int	*arr = ft_rrange(atoi(av[1]), atoi(av[2]));
+	int	*arrptr = arr;
+	int	i = 0;
+	while (i < ft_abs(atoi(av[1]), atoi(av[2])))
+	{
+		printf("%d ", *arrptr++);
+		i++;
+	}
+	return (free(arr), 0);
+} */
+
+/*
+Assignment name  : ft_rrange
+Expected files   : ft_rrange.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+Write the following function:
+int     *ft_rrange(int start, int end);
+It must allocate (with malloc()) an array of integers, fill it with consecutive
+values that begin at end and end at start (Including start and end !), then
+return a pointer to the first value of the array.
+Examples:
+- With (1, 3) you will return an array containing 3, 2 and 1
+- With (-1, 2) you will return an array containing 2, 1, 0 and -1.
+- With (0, 0) you will return an array containing 0.
+- With (0, -3) you will return an array containing -3, -2, -1 and 0. */
---
+++ ExamPractice/Rank02/Lvl03/hidenp.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hidenp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 11:05:45 by cwan              #+#    #+#             */
+/*   Updated: 2023/12/20 11:42:37 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	main(int ac, char *av[])
+{
+	int	i = 0;
+	int j = 0;
+	while (ac == 3 && av[1][i])
+	{
+		while (av[2][j])
+		{
+			if (av[1][i] == av[2][j])
+				i++;
+			j++;
+		}
+		if (!av[1][i])
+			return (write(1, "1\n", 2), 0);
+		else
+			return (write(1, "0\n", 2), 0);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
+
+/*
+Assignment name  : hidenp
+Expected files   : hidenp.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+Write a program named hidenp that takes two strings and displays 1
+followed by a newline if the first string is hidden in the second one,
+otherwise displays 0 followed by a newline.
+Let s1 and s2 be strings. We say that s1 is hidden in s2 if it's possible to
+find each character from s1 in s2, in the same order as they appear in s1.
+Also, the empty string is hidden in any string.
+If the number of parameters is not 2, the program displays a newline.
+Examples :
+$>./hidenp "fgex.;" "tyf34gdgf;'ektufjhgdgex.;.;rtjynur6" | cat -e
+1$
+$>./hidenp "abc" "2altrb53c.sse" | cat -e
+1$
+$>./hidenp "abc" "btarc" | cat -e
+0$
+$>./hidenp | cat -e
+$
+$> */
