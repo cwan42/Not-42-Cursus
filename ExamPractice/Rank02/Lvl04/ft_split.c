76222b3c0bd14772e4ddd6da09bb004ec0e417a9
cwan <cwan@c2r6s8.42singapore.sg>
2023-12-28 11:52:29 +0800

    Added ft_split.c hardcoded to 100 array size w 100 bytes ea

--- ExamPractice/Rank02/Lvl04/ft_split.c
+++ ExamPractice/Rank02/Lvl04/ft_split.c
@@ -6,10 +6,66 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 09:44:58 by cwan              #+#    #+#             */
-/*   Updated: 2023/12/28 09:45:11 by cwan             ###   ########.fr       */
+/*   Updated: 2023/12/28 11:52:20 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+char	**ft_split(char *str)
+{
+	int		i = 0;
+	int		j = 0;
+	int		k;
+	char	**array = (malloc(sizeof(**array) * 100));
+
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i++;
+	while (str[i])
+	{
+		if (str[i] > 32)
+		{
+			k = 0;
+			array[j] = malloc(sizeof(char) * 100);
+			while (str[i] > 32)
+				array[j][k++] = str[i++];
+		array[j][k] = '\0';
+		j++;
+		}
+		else
+			i++;
+	}
+	array[j] = 0;
+	return (array);
+}
+/*
+#include <stdio.h>
+
+int	main(int ac, char *av[])
+{
+	char	**array;
+	int 	i = 0;
+
+	if (ac == 2)
+		array = ft_split(av[1]);
+	else
+		return (0);
+	while (array[i])
+	{
+		if (!array[i + 1])
+		{
+			printf("%s.\n", array[i]);
+			break ;
+		}
+		printf("%s, ", array[i]);
+		i++;
+	}
+	i = 0;
+	while (array[i])
+		free(array[i++]);
+	return (free(array), 0);
+}
+*/
 /*
 Assignment name  : ft_split
 Expected files   : ft_split.c
