dfde44e8a7f4add06851ecd81d316314dfe6cf59
cwan <cwan@c2r6s8.42singapore.sg>
2023-12-28 11:49:59 +0800

    Added sort_int_tab.c

--- ExamPractice/Rank02/Lvl04/sort_int_tab.c
+++ ExamPractice/Rank02/Lvl04/sort_int_tab.c
@@ -6,10 +6,54 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 09:46:27 by cwan              #+#    #+#             */
-/*   Updated: 2023/12/28 09:46:38 by cwan             ###   ########.fr       */
+/*   Updated: 2023/12/28 11:48:59 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i;
+	int				tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+			i = -1;
+		}
+		i++;
+	}
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char *av[])
+{
+	int	size = ac - 1;
+	int	*tab = malloc(size * sizeof(int));
+	int	i = 0;
+
+	while (i < size)
+	{
+		tab[i] = atoi(av[i + 1]);
+		i++;
+	}
+	i = 0;
+	while (i < size)
+		printf("%d ", tab[i++]);
+	printf("\n");
+	sort_int_tab(tab, size);
+	i = 0;
+	while (i < size)
+		printf("%d ", tab[i++]);
+	return (printf("\n"), free(tab), 0);
+}
+*/
 /*
 Assignment name  : sort_int_tab
 Expected files   : sort_int_tab.c
