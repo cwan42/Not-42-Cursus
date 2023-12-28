e6d4ed23244eb0d24fcd7914ed5827600d6b12d0
cwan <cwan@c2r6s8.42singapore.sg>
2023-12-28 13:32:42 +0800

    Added ft_list_foreach.c

--- ExamPractice/Rank02/Lvl04/ft_list_foreach.c
+++ ExamPractice/Rank02/Lvl04/ft_list_foreach.c
@@ -6,10 +6,77 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 09:43:48 by cwan              #+#    #+#             */
-/*   Updated: 2023/12/28 09:44:02 by cwan             ###   ########.fr       */
+/*   Updated: 2023/12/28 13:32:29 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list	*node;
+
+	node = begin_list;
+	while (node)
+	{
+		(*f)(node->data);
+		node = node->next;
+	}
+}
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+void	printint(void *data)
+{
+	printf("%d ", *(int *)data);
+}
+
+t_list	*createnode(void *data)
+{
+	t_list	*newnode;
+
+	newnode = (t_list*)malloc(sizeof(t_list));
+	if (newnode)
+	{
+		newnode->data = data;
+		newnode->next = NULL;
+	}
+	return (newnode);
+}
+
+int	main(void)
+{
+	t_list *list = NULL;
+	t_list *newnode;
+	t_list *temp;
+	int	array[] = {1, 3, 5, 2, 4, 6};
+	int	i = 0;
+
+	while (i < 6)
+	{
+		newnode = createnode((void *)&array[i]);
+		if (!list)
+			list = newnode;
+		else
+		{
+			temp = list;
+			while (temp->next)
+				temp = temp->next;
+			temp->next = newnode;
+		}		
+		i++;
+	}
+	ft_list_foreach(list, &printint);
+	while (list)
+	{
+		temp = list;
+		list = list->next;
+		free(temp);
+	}
+	return (0);
+}
+*/
 /*
 Assignment name  : ft_list_foreach
 Expected files   : ft_list_foreach.c, ft_list.h
