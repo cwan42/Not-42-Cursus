a6239f1f1eabb9058fe4fbcd3a8b7f64743ceafa
cwan <cwan@c2r6s8.42singapore.sg>
2023-12-28 09:47:19 +0800

    Added subject wip for all Lvl04 qns

---
+++ ExamPractice/Rank02/Lvl04/ft_itoa.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 09:43:08 by cwan              #+#    #+#             */
+/*   Updated: 2023/12/28 09:43:28 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+/*
+Assignment name  : ft_itoa
+Expected files   : ft_itoa.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+Write a function that takes an int and converts it to a null-terminated string.
+The function returns the result in a char array that you must allocate.
+Your function must be declared as follows:
+char	*ft_itoa(int nbr); */
---
+++ ExamPractice/Rank02/Lvl04/ft_split.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 09:44:58 by cwan              #+#    #+#             */
+/*   Updated: 2023/12/28 09:45:11 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : ft_split
+Expected files   : ft_split.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+Write a function that takes a string, splits it into words, and returns them as
+a NULL-terminated array of strings.
+A "word" is defined as a part of a string delimited either by spaces/tabs/new
+lines, or by the start/end of the string.
+Your function must be declared as follows:
+char    **ft_split(char *str); */
