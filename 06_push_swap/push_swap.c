41d85f7f0dedef240d7d64e016735bb4ec866ae5
cwan <cwan@c2r6s8.42singapore.sg>
2024-01-02 15:07:28 +0800

    Updated push_swap c and h

--- 06_push_swap/push_swap.c
+++ 06_push_swap/push_swap.c
@@ -6,14 +6,58 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:04 by cwan              #+#    #+#             */
-/*   Updated: 2023/12/08 16:55:20 by cwan             ###   ########.fr       */
+/*   Updated: 2024/01/02 12:56:21 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(void)
+#include <stdio.h>
+
+void	ft_createstacka(char **av, t_list **stack)
 {
-	ft_printf("Hello!");
+	t_list *node;
+
+	while (*av)
+	{
+		node = ft_lstnew(ft_atoi(*av));
+		if (!node)
+			break ;
+		ft_lstadd_back(stack, node);
+		av++;
+	}
+}
+
+int	main(int ac, char *av[])
+{
+	t_list	*a;
+	t_list	*b;
+
+	a = NULL;
+	b = NULL;
+	if (ac < 2 || !av[1])
+		return (1);
+	else if (ac == 2)
+		av = ft_split(av[1], ' ');
+	int	i = 1;
+	while (av[i])
+	{
+		if (!av[i + 1])
+		{
+			printf("%s", av[i++]);
+			break ; 
+		}
+		printf("%s, ", av[i++]);
+	}
+	ft_createstacka(av, &a);
+	t_list *cur = a;
+	printf("\n");
+	while (cur)
+	{
+		printf("%d ", cur->data);
+		cur = cur->next;
+	}
+	printf("\n");
+	ft_lstclear(&a, free);
 	return (0);
 }
--- 06_push_swap/push_swap.h
+++ 06_push_swap/push_swap.h
@@ -6,24 +6,25 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:51 by cwan              #+#    #+#             */
-/*   Updated: 2023/12/14 16:00:20 by cwan             ###   ########.fr       */
+/*   Updated: 2024/01/02 13:08:50 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+typedef struct s_stack
+{
+	int				value;
+	int				pos;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}					t_list;
+
 # include "./libft/libft.h"
 # include "./ft_printf/ft_printf.h"
 
 # include <unistd.h>
 # include <stdlib.h>
-
-typedef struct s_stack
-{
-	int				value;
-	struct s_stack	*next;
-	struct s_stack	*prev;
-}					t_stack;
 
 #endif
