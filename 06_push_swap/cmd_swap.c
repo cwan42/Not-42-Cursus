9baa2078a85501fa144e48bd540934bb19825ad1
cwan <cwan@c2r6s8.42singapore.sg>
2024-01-03 13:28:36 +0800

    Updated push_swap h and c, currently need to fix cmd_swap.c to swap node double pointers correctly.

--- 06_push_swap/cmd_swap.c
+++ 06_push_swap/cmd_swap.c
@@ -6,15 +6,15 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 10:30:21 by cwan              #+#    #+#             */
-/*   Updated: 2024/01/03 12:57:37 by cwan             ###   ########.fr       */
+/*   Updated: 2024/01/03 13:27:48 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/*
 static void	swap(t_stack *node)
 {
-/*
 	int	tmp;
 
 	if (!node || !node->next)
@@ -23,38 +23,39 @@ static void	swap(t_stack *node)
 	node->value = node->next->value;
 	node->next->value = tmp;
 */
-/*	if (*node && (*node)->next)
-	{
-		*node = (*node)->next;
-		(*node)->prev->prev = *node;
-		(*node)->prev->next = (*node)->next;
-		if ((*node)->next)
-			(*node)->next->prev = (*node)->prev;
-		(*node)->next = (*node)->prev;
-		(*node)->prev = NULL;
-	}*/
+static void	swap(t_stack **node)
+{
+	if (!*node || !(*node)->next)
+		return ;
+	*node = (*node)->next;
+	(*node)->prev->prev = *node;
+	(*node)->prev->next = (*node)->next;
+	if ((*node)->next)
+		(*node)->next->prev = (*node)->prev;
+	(*node)->next = (*node)->prev;
+	(*node)->prev = NULL;
 }
 
-void	sa(t_stack *a)
+void	sa(t_stack **a)
 {
-	if (a && a->next)
+	if (*a && (*a)->next)
 	{
-	ft_printf("B4 swap: (*a)->value = %d, (*a)->next->value = %d\n", a->value, a->next->value);
+	ft_printf("B4 swap: (*a)->value = %d, (*a)->next->value = %d\n", (*a)->value, (*a)->next->value);
 	swap(a);
-	ft_printf("After swap: (*a)->value = %d, (*a)->next->value = %d\n", a->value, a->next->value);
+	ft_printf("After swap: (*a)->value = %d, (*a)->next->value = %d\n", (*a)->value, (*a)->next->value);
 	ft_printf("sa\n");
 	}
 }
 
 void	sb(t_stack *b)
 {
-	swap(b);
+	swap(&b);
 	ft_printf("sb\n");
 }
 
 void	ss(t_stack *a, t_stack *b)
 {
-	swap(a);
-	swap(b);
+	swap(&a);
+	swap(&b);
 	ft_printf("ss\n");
 }
--- 06_push_swap/push_swap.h
+++ 06_push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:51 by cwan              #+#    #+#             */
-/*   Updated: 2024/01/03 12:41:04 by cwan             ###   ########.fr       */
+/*   Updated: 2024/01/03 13:18:03 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ t_stack	*ft_stacknew(int value);
 void	ft_stackadd_back(t_stack **stack, t_stack *new);
 t_stack	*ft_stacklast(t_stack *stack);
 
-void	sa(t_stack *a);
+void	sa(t_stack **a);
 void	sb(t_stack *b);
 void	ss(t_stack *a, t_stack *b);
 
