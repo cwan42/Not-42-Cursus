770ee3630c800923d2c23d5e4e350133a3b13b8f
cwan <cwan@c2r6s8.42singapore.sg>
2024-01-05 16:26:29 +0800

    Updated flood_fill.c w main and t_point program

--- ExamPractice/Rank02/Lvl04/flood_fill.c
+++ ExamPractice/Rank02/Lvl04/flood_fill.c
@@ -6,9 +6,10 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 14:03:42 by cwan              #+#    #+#             */
-/*   Updated: 2023/12/26 14:41:21 by cwan             ###   ########.fr       */
+/*   Updated: 2024/01/05 16:20:52 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 typedef struct s_point
 {
@@ -17,6 +18,50 @@ typedef struct s_point
 }				t_point;
 
 void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	
+}
+
+#include <stdlib.h>
+#include <stdio.h>
+//#include "flood_fill.h" not necessary cos' it's on top
+
+char**	make_area(char ** zone, t_point size)
+{
+	char**	new;
+
+	new = malloc(sizeof(char*) * size.y);
+	for (int i = 0; i < size.y; ++i)
+	{
+		new[i] = malloc(size.x + 1);
+		for (int j = 0; j < size.x; ++j)
+			new[i][j] = zone[i][j];
+		new[i][size.x] = '\0';
+	}
+	return (new);
+}
+
+int	main(void)
+{
+	t_point size = {8, 5};
+	char	*zone[] = {
+			"11111111",
+			"10001001",
+			"10010001",
+			"10110001",
+			"11100001",};
+
+	char**	area = make_area(zone, size);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	printf("\n");
+
+	t_point	begin = {7, 4};
+	flood_fill(area, size, begin);
+	for (int i = 0; i < size.y; ++i)
+		printf("%s\n", area[i]);
+	return (0);
+}
 
 /*
 Assignment name  : flood_fill
