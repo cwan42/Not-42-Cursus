eef4362e5ffa846d3f36100c8069b0afadfaa3bc
cwan <cwan@c2r6s8.42singapore.sg>
2024-01-08 16:37:36 +0800

    Refactored rev_print.c

--- ExamPractice/Rank02/Lvl01/rev_print.c
+++ ExamPractice/Rank02/Lvl01/rev_print.c
@@ -6,10 +6,10 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:19:41 by cwan              #+#    #+#             */
-/*   Updated: 2023/11/10 16:39:25 by cwan             ###   ########.fr       */
+/*   Updated: 2024/01/08 16:36:47 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
 #include <unistd.h>
 
 int	main(int ac, char *av[])
@@ -27,6 +27,22 @@ int	main(int ac, char *av[])
 	while (i > 0)
 		write(1, &av[1][--i], 1);
 	write(1,"\n", 1);
+}*/
+
+#include <unistd.h>
+
+int	main(int ac, char *av[])
+{
+	int	i = 0;
+
+	if (ac == 2)
+	{
+		while (av[1][i])
+			i++;
+		while (i > 0)
+			write(1, &av[1][--i], 1);
+	}
+	return (write(1, "\n", 1), 0);
 }
 
 /*
