99bf4842ce4818c4bd08bf6306bbfc164ba19f9b
cwan <cwan@c2r6s8.42singapore.sg>
2024-01-08 15:54:48 +0800

    Refactored ft_strcpy.c

--- ExamPractice/Rank02/Lvl01/ft_strcpy.c
+++ ExamPractice/Rank02/Lvl01/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 14:03:28 by cwan              #+#    #+#             */
-/*   Updated: 2023/11/16 11:53:20 by cwan             ###   ########.fr       */
+/*   Updated: 2024/01/08 15:54:20 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,28 @@ char	*ft_strcpy(char *s1, char *s2)
 
 	i = 0;
 	while (s2[i])
-		s1[i] = s2[i++];
+	{
+		s1[i] = s2[i];
+		i++;
+	}
 	s1[++i] = '\0';
 	return (s1);
 }
+/*
+char	*ft_strcpy(char *s1, char *s2)
+{
+	char *tmp = s1;
 
+	while (*s2)
+	{
+		*s1 = *s2;
+		s1++;
+		s2++;
+	}
+	*s1 = '\0';
+	return (tmp);
+}*/
+/*
 #include <stdio.h>
 
 int	main(void)
@@ -29,7 +46,7 @@ int	main(void)
 	printf("%s\n", (ft_strcpy(s1, "test")));
 	printf("%s\n", (ft_strcpy(s1, "testing \t\t\t\t\t\t\t\ttest")));
 	printf("%s\n", (ft_strcpy(s1, "longtest 2234osifsj \n \t \r test")));
-}
+}*/
 
 /*
 Assignment name  : ft_strcpy
