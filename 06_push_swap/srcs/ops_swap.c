eea0f2019bb4082bcb24d38586d01eccb28768c4
cwan <cwan@c1r8s4.42singapore.sg>
2024-02-22 16:21:09 +0800

    Updated Makefile, created srcs/ for ops files

---
+++ 06_push_swap/srcs/ops_swap.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ops_swap.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 16:13:43 by cwan              #+#    #+#             */
+/*   Updated: 2024/02/22 16:20:09 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.c"
+
+void	sa(t_stack **heada)
+{
+	swap(heada);
+	ft_printf("sa\n");
+}
+
+void	sb(t_stack **headb)
+{
+	swap(headb);
+	ft_printf("sb\n");
+}
+
+void	ss(t_stack **heada, t_stack **headb)
+{
+	swap(heada);
+	swap(headb);
+	ft_printf("ss\n");
+}
+
+void	swap(t_stack **head)
+{
+	t_stack	*first;
+	t_stack	*second;
+
+	if (!*head || (!(*head)->next))
+		return ;
+	first = *head;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	*head = second;
+}
