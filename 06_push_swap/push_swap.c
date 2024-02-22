3a93d3d7e8af3a5a9761d6c191789d4e4114b359
cwan <cwan@c1r8s4.42singapore.sg>
2024-02-22 15:47:06 +0800

    Restarted push_swap files

--- 06_push_swap/push_swap.c
+++ 06_push_swap/push_swap.c
@@ -6,48 +6,9 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:04 by cwan              #+#    #+#             */
-/*   Updated: 2024/01/03 13:27:12 by cwan             ###   ########.fr       */
+/*   Updated: 2024/02/22 15:46:42 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_createstacka(char **av, t_stack **stack, int ac)
-{
-	t_stack	*node;
-
-	if (ac != 2)
-		av++;
-	while (*av)
-	{
-		node = ft_stacknew(ft_atoi(*av));
-		if (!node)
-			break ;
-		ft_stackadd_back(stack, node);
-		av++;
-	}
-}
-
-int	main(int ac, char *av[])
-{
-	t_stack	*a;
-	t_stack	*b;
-	t_stack	*cur;
-
-	a = NULL;
-	b = NULL;
-	if (ac < 2 || !av[1][0])
-		return (1);
-	else if (ac == 2)
-		av = ft_split(av[1], ' ');
-	ft_createstacka(av, &a, ac);
-	cur = a;
-	sa(&cur);
-	while (cur)
-	{
-		ft_printf("%d ", cur->value);
-		cur = cur->next;
-	}
-	ft_printf("\n");
-	return (0);
-}
--- 06_push_swap/push_swap.h
+++ 06_push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:51 by cwan              #+#    #+#             */
-/*   Updated: 2024/01/03 13:18:03 by cwan             ###   ########.fr       */
+/*   Updated: 2024/02/22 15:46:17 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,8 @@
 typedef struct s_stack
 {
 	int				value;
-	int				pos;
 	struct s_stack	*next;
 	struct s_stack	*prev;
 }					t_stack;
-
-t_stack	*ft_stacknew(int value);
-void	ft_stackadd_back(t_stack **stack, t_stack *new);
-t_stack	*ft_stacklast(t_stack *stack);
-
-void	sa(t_stack **a);
-void	sb(t_stack *b);
-void	ss(t_stack *a, t_stack *b);
 
 #endif
