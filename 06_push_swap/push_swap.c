08e5f8472ad9cbee08a664c56c1dca9f922e2a3f
cwan <cwan@c1r8s4.42singapore.sg>
2024-02-23 18:30:09 +0800

    updated push_swap.c to init a list

--- 06_push_swap/push_swap.c
+++ 06_push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:04 by cwan              #+#    #+#             */
-/*   Updated: 2024/02/23 17:39:10 by cwan             ###   ########.fr       */
+/*   Updated: 2024/02/23 18:29:08 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,27 @@ int	main(void)
 {
 	t_stack *list = NULL;
 	t_stack	*newnode;
-//	t_list	*temp = 0;
+	t_stack	*head;
 	int		array[] = {3, 5, 2, 1, 4};
 	int		i = 0;
 
 	while (i < 5)
 	{
-		newnode = ft_stknew(array[i]);
+		newnode = ft_stknew(array[i++]);
 		if (!list)
 			list = newnode;
 		else
 		{
-			
+			ft_stkadd_back(&list, newnode);
 		}
 	}
+	head = list;
+	while (list->next != head)
+	{
+		ft_printf("%d ", list->num);
+		list = list->next;
+	}
+	ft_printf("%d.", list->num);
+	ft_freestack(&list, head);
+	return (0);
 }
