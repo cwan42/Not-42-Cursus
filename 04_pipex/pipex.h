2c4ac25f902ae1c0c52118e526b92486c91e49b0
cwan <cwan@c1r8s4.42singapore.sg>
2024-02-24 11:59:27 +0800

    Updated Makefile to remove bonus, pipex.h to remove unneeded headers, removed pipexbonus.c

--- 04_pipex/pipex.h
+++ 04_pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 10:56:43 by cwan              #+#    #+#             */
-/*   Updated: 2023/12/07 19:16:57 by cwan             ###   ########.fr       */
+/*   Updated: 2024/02/24 11:58:06 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,5 @@
 
 # include <sys/wait.h>
 // for wait
-
-# include <errno.h>
-// for errno values used in perror
-
-# include <stdio.h>
-// for perror
 
 #endif
