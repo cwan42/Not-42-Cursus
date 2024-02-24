50f60afd73e0a64d4a3d65597cb1fd5d762a4d24
cwan <cwan@c1r8s4.42singapore.sg>
2024-02-24 16:36:20 +0800

    Updated push_swap.c

--- 06_push_swap/push_swap.c
+++ 06_push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:04 by cwan              #+#    #+#             */
-/*   Updated: 2024/02/23 18:29:08 by cwan             ###   ########.fr       */
+/*   Updated: 2024/02/24 16:35:48 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,28 @@
 
 int	main(void)
 {
-	t_stack *list = NULL;
+	t_stack **list = NULL;
 	t_stack	*newnode;
 	t_stack	*head;
 	int		array[] = {3, 5, 2, 1, 4};
 	int		i = 0;
 
+	list = malloc(sizeof(t_stack *));
 	while (i < 5)
 	{
 		newnode = ft_stknew(array[i++]);
 		if (!list)
-			list = newnode;
+			*list = newnode;
 		else
-		{
-			ft_stkadd_back(&list, newnode);
-		}
+			ft_stkadd_back(list, newnode);
 	}
-	head = list;
-	while (list->next != head)
+	head = *list;
+	while ((*list)->next != head)
 	{
-		ft_printf("%d ", list->num);
-		list = list->next;
+		ft_printf("%d ", (*list)->num);
+		*list = (*list)->next;
 	}
-	ft_printf("%d.", list->num);
-	ft_freestack(&list, head);
+	ft_printf("%d.\n", (*list)->num);
+	ft_freestack(list, head);
 	return (0);
 }
