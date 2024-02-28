4dc81a30187dfe74cc7470a9ce40402b8c9d9bf5
cwan <cwan@c1r8s4.42singapore.sg>
2024-02-28 16:41:23 +0800

    Not working updated push_swap.c

--- 06_push_swap/push_swap.c
+++ 06_push_swap/push_swap.c
@@ -6,30 +6,14 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:04 by cwan              #+#    #+#             */
-/*   Updated: 2024/02/27 18:27:29 by cwan             ###   ########.fr       */
+/*   Updated: 2024/02/28 16:40:56 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(void)
-{
-	t_stack	**list = NULL;
-	t_stack	*head;
-	int		array[] = {5};
-	int		i = 0;
+#include <sys/wait.h>
 
-	list = malloc(sizeof(t_stack *));
-	*list = ft_stknew(array[i]);
-	if (!*list)
-		return (free(list), 1);
-	head = *list;
-	ft_printf("The number is %d.\n", (*list)->num);
-	ft_freestack(list, head);
-	return (0);
-}
-
-/*
 int	main(void)
 {
 	t_stack **list = NULL;
@@ -38,18 +22,28 @@ int	main(void)
 	int		array[] = {3, 5, 2, 1, 4};
 	int		i = 0;
 
-	list = malloc(sizeof(t_stack *) * 6);
+	list = malloc(sizeof(t_stack *));
 	if (!list)
-		return (free(list), 1);
+		return (1);
+	*list = NULL;
 	while (i < 5)
 	{
 		newnode = ft_stknew(array[i++]);
-		if (!*list)
-			*list = newnode;
-		else
-			ft_stkadd_back(list, newnode);
+		if (!newnode)
+			return (ft_freestack(list, *list), 1);
+		ft_stkadd_back(list, newnode);
 	}
-	head = *list;
+	if (*list)
+		head = *list;
+	while ((*list)->next != head)
+	{
+		ft_printf("%d ", (*list)->num);
+		*list = (*list)->next;
+	}
+	ft_printf("%d.\n", (*list)->num);
+	wait(&i);
+	*list = head;
+	sa(list);
 	while ((*list)->next != head)
 	{
 		ft_printf("%d ", (*list)->num);
@@ -58,4 +52,4 @@ int	main(void)
 	ft_printf("%d.\n", (*list)->num);
 	ft_freestack(list, head);
 	return (0);
-}*/
+}
