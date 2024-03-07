b9fe09fe7b6b5aa4acc7bcc3f689c75322e02220
cwan <cwan@c1r8s4.42singapore.sg>
2024-03-07 19:59:57 +0800

    Updated ops_push.c, works as long as stacks > 1. Need to include if conditions for init when stack = 0 or 1.

--- 06_push_swap/srcs/ops_push.c
+++ 06_push_swap/srcs/ops_push.c
@@ -6,8 +6,40 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 16:13:32 by cwan              #+#    #+#             */
-/*   Updated: 2024/02/22 16:13:40 by cwan             ###   ########.fr       */
+/*   Updated: 2024/03/07 19:55:24 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	pa(t_stack **heada, t_stack **headb)
+{
+	push(headb, heada);
+	ft_printf("pa\n");
+}
+
+void	pb(t_stack **heada, t_stack **headb)
+{
+	push(heada, headb);
+	ft_printf("pb\n");
+}
+
+void	push(t_stack **heada, t_stack **headb)
+{
+	t_stack *tmp;
+	t_stack	*tmp2;
+
+	if (!*heada || !*headb)
+		return ;
+	tmp = *heada;
+	*heada = (*heada)->next;
+	(*heada)->prev = tmp->prev;
+	tmp->prev->next = *heada;
+
+	tmp2 = *headb;
+	*headb = tmp;
+	(*headb)->next = tmp2;
+	(*headb)->prev = tmp2->prev;
+	tmp2->prev->next = *headb;
+	tmp2->next->prev = *headb;
+}
