38b50179a318fb77a6d7b7bf05dfc8768c6850f6
cwan <cwan@c1r8s4.42singapore.sg>
2024-03-13 12:17:56 +0800

    Updated push_swap.c for init stack and error check

--- 06_push_swap/push_swap.c
+++ 06_push_swap/push_swap.c
@@ -6,48 +6,59 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 16:49:04 by cwan              #+#    #+#             */
-/*   Updated: 2024/03/11 16:24:48 by cwan             ###   ########.fr       */
+/*   Updated: 2024/03/13 12:16:03 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	**convcheck(char **av)
+int	inputcheck(char **av, t_stack **list)
 {
-	int		i;
 	char	*wspace;
 	char	*str;
 	char	**arr;
-	t_stack	**lista;
+	int		i;
 
-	i = 1;
 	str = malloc(1);
 	str = " ";
-	while (av[i])
+	av++;
+	while (*av)
 	{
-		wspace = ft_strjoin(av[i++], " ");
+		wspace = ft_strjoin(*av++, " ");
 		str = ft_strjoin(str, wspace);
 		free(wspace);
 	}
 	i = 0;
-	while ((str[i] >= '0' && str[i] <= '9') || str[i] == ' ')
-// TEST FOR NON INTEGERS
+	while ((str[i] >= '0' && str[i] <= '9') || str[i] == ' ' || str[i] == '\n')
 		i++;
-	arr = ft_split(str, ' ');
-//	SPLIT ARRAY test for dup
+	arr = ft_split((str), ' ');
+	while (*arr)
+		ft_stkadd_back(list, ft_stknew(ft_atoi(*arr++)));
 	if (str[i])
-		return (ft_putstr_fd("Error\n", 2), NULL);
-	return (lista);
+		return (free(str), 1);
+	return (0);
 }
+/*
+//	ARRAY test for duplicates, MININT/MAXINT
+}*/
 
 int	main(int ac, char *av[])
 {
 	t_stack	**list = NULL;
-	t_stack *head = *list;
+//	t_stack *head = *list;
 
+	list = malloc(sizeof(t_stack *));
 	if (ac > 1)
-		list = convcheck(av);
-	if (*list)
+	{
+		if (inputcheck(av, list))
+			return (ft_putstr_fd("Error\n", 2), 1);
+		else
+			ft_printf("%d\n", (*list)->num);
+	}
+	return (0);
+}
+	
+/*f (*list)
 	{
 		while (head->next != *list)
 		{
@@ -55,7 +66,7 @@ int	main(int ac, char *av[])
 			head = head->next;
 		}
 		ft_printf("%d.\t", head->num);
-	}	
+	}	*/
 /*
 	char	**array;
 	int		i;
@@ -76,7 +87,6 @@ int	main(int ac, char *av[])
 	no param -> display nothing and return prompt
 	if error, display "Error" \n on stderr. (if arg != integer, > MAXINT or dup
 */
-}
 /*
 void	printloops(t_stack **list, t_stack **listb)
 {
