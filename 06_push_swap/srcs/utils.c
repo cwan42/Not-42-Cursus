d262130abaaf38dd18d5139578976a586c8e62f1
cwan <cwan@c1r8s4.42singapore.sg>
2024-03-16 20:25:40 +0800

    Added ft_strcmp to utils.c

--- 06_push_swap/srcs/utils.c
+++ 06_push_swap/srcs/utils.c
@@ -6,11 +6,27 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 11:43:01 by cwan              #+#    #+#             */
-/*   Updated: 2024/03/13 11:45:35 by cwan             ###   ########.fr       */
+/*   Updated: 2024/03/16 19:42:37 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (1); 
+		i++;
+	}
+	if (s1[i] || s2[i])
+		return (1);
+	return (0);
+}
 
 void	ft_free(char **arr)
 {
