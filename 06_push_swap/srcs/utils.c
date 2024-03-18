0a6e01c148cc44787fb3e897c9c38bef838502da
cwan <cwan@c1r8s4.42singapore.sg>
2024-03-18 15:52:03 +0800

    Added ft_atol into utils, edited for norme

--- 06_push_swap/srcs/utils.c
+++ 06_push_swap/srcs/utils.c
@@ -6,11 +6,34 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 11:43:01 by cwan              #+#    #+#             */
-/*   Updated: 2024/03/16 19:42:37 by cwan             ###   ########.fr       */
+/*   Updated: 2024/03/18 15:46:14 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+long	ft_atol(const char *str)
+{
+	long	i;
+	long	sign;
+	long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || (str[i] == ' '))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] && str[i] <= '9' && str[i] >= '0')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
@@ -20,7 +43,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
-			return (1); 
+			return (1);
 		i++;
 	}
 	if (s1[i] || s2[i])
