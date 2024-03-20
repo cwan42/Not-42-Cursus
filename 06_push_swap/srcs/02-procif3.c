92c48c420af3df1ed1000b4f5564ebb72b436edd
cwan <cwan@c1r3s3.42singapore.sg>
2024-03-20 21:53:54 +0800

    Added initif3, now doing if 4 or 5

--- 06_push_swap/srcs/02-procif3.c
+++ 06_push_swap/srcs/02-procif3.c
@@ -6,7 +6,7 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:23:31 by cwan              #+#    #+#             */
-/*   Updated: 2024/03/20 20:11:14 by cwan             ###   ########.fr       */
+/*   Updated: 2024/03/20 21:52:12 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,28 @@ void	initif3(t_stack **a, t_stack **b)
 {
 	if (!a || !b)
 		return ;
-	if ((*a)->nu > (*a)->n->nu && (*a)->n->nu < (*a)->p->nu)
+	if ((*a)->nu < (*a)->n->nu && (*a)->n->nu < (*a)->p->nu)
+		return ;
+	if ((*a)->nu < (*a)->n->nu && (*a)->nu < (*a)->p->nu)
+		(void)sa(a),ra(a);
+	else if ((*a)->nu > (*a)->n->nu && (*a)->nu < (*a)->p->nu)
 		sa(a);
-	ft_printf("initif3 is running\n");
-//	else if (*a
+	else if ((*a)->nu < (*a)->n->nu && (*a)->nu > (*a)->p->nu)
+		rra(a);
+	else if ((*a)->nu > (*a)->n->nu && (*a)->n->nu < (*a)->p->nu)
+		ra(a);
+	else if ((*a)->nu > (*a)->n->nu && (*a)->n->nu > (*a)->p->nu)
+		(void)sa(a),rra(a);
+}
+
+void	initif4or5(t_stack **a, t_stack **b)
+{
+	if (!a || !b)
+		return ;
+	while ((*a)->n->n->n->nu != (*a)->nu)
+		pb(a, b);
+	initif3(a, b);
+//	if ((*b)->nu < (ft_stklast(*a)->nu))
 }
 
 int	initpri(t_stack **a, t_stack **b)
@@ -46,5 +64,7 @@ int	initpri(t_stack **a, t_stack **b)
 	size = stacksize(a);
 	if (size == 3)
 		initif3(a, b);
+	else if (size > 3 && size <6)
+		initif4or5(a, b);
 	return (0);
 }
