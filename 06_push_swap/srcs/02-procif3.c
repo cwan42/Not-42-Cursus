8f709cedd8a6834b94597a5721bab996a8c3829d
cwan <cwan@c1r8s4.42singapore.sg>
2024-03-22 06:14:03 +0800

    Updated 02-procif3.c with helper functions, currently working on stacksorted fn, need to figure out how to end the loop of while (*a)

--- 06_push_swap/srcs/02-procif3.c
+++ 06_push_swap/srcs/02-procif3.c
@@ -6,11 +6,43 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:23:31 by cwan              #+#    #+#             */
-/*   Updated: 2024/03/21 20:35:51 by cwan42           ###   ########.fr       */
+/*   Updated: 2024/03/22 06:07:32 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	numax(t_stack **a)
+{
+	t_stack	*tmp;
+	int		i;
+
+	tmp = *a;
+	i = tmp->p->nu;
+	while (tmp->n != *a)
+	{
+		if (tmp->nu > i)
+			i = tmp->nu;
+		tmp = tmp->n;
+	}
+	return (i);
+}
+
+int	numin(t_stack **a)
+{
+	t_stack	*tmp;
+	int		i;
+
+	tmp = *a;
+	i = tmp->p->nu;
+	while (tmp->n != *a)
+	{
+		if (tmp->nu < i)
+			i = tmp->nu;
+		tmp = tmp->n;
+	}
+	return (i);
+}
 
 int	stacksize(t_stack **a)
 {
@@ -27,61 +59,62 @@ int	stacksize(t_stack **a)
 	return (count);
 }
 
-int	issmol(t_stack **a)
+int	stacksorted(t_stack	**a)
 {
 	t_stack	*tmp;
+	int		i;
 
 	tmp = *a;
-
-	if (!a || !*a)
-		return (2);
-	while (*a && tmp->n->nu != (*a)->nu)
+	i = 0;
+	while (i != stacksize(a) && tmp->nu != (*a)->nu)
 	{
-		if (tmp->nu < tmp->n->nu)
-			tmp = tmp->n;
-		else
-			return (ft_printf("issmol=1"), 1);
+		i++;
+		tmp = tmp->n;
+		ft_printf("%d\n", i);
+		if (tmp->nu > tmp->n->nu)
+			return (1);
 	}
-	return (ft_printf("issmol=0"), 0);
-}
-void	init2or3(t_stack **a)
+	return (0);
+} // need to fix, prob cos using circular list so need to find way to temrinate
+
+t_stack	*mediannode(t_stack **a)
 {
-	if (!a || !*a)
-		return ;
-	if (((*a)->n == *a) || ((*a)->n->n == *a && (*a)->n->n == \
-	*a && (*a)->nu < (*a)->n->nu))
-		return;
-	else if ((*a)->n->n == *a && (*a)->nu > (*a)->n->nu)
-		sa(a);
-	else if ((*a)->nu < (*a)->n->nu && (*a)->n->nu < (*a)->p->nu)
-		return ;
-	else if ((*a)->nu < (*a)->n->nu && (*a)->nu < (*a)->p->nu)
-		(void)sa(a),ra(a);
-	else if ((*a)->nu > (*a)->n->nu && (*a)->nu < (*a)->p->nu)
-		sa(a);
-	else if ((*a)->nu < (*a)->n->nu && (*a)->nu > (*a)->p->nu)
-		rra(a);
-	else if ((*a)->nu > (*a)->n->nu && (*a)->n->nu < (*a)->p->nu)
-		ra(a);
-	else if ((*a)->nu > (*a)->n->nu && (*a)->n->nu > (*a)->p->nu)
-		(void)sa(a),rra(a);
+	t_stack	*tmp;
+	int		i;
+
+	tmp = *a;
+	i = 0;	
+	while (i < (stacksize(a) / 2))
+	{
+		tmp = tmp->n;
+		i++;
+	}
+	return (tmp);
 }
 
-void	initif4or5(t_stack **a, t_stack **b)
+void	init3(t_stack **a)
+{
+	ft_printf("Stacksorted is %d\n", stacksorted(a));
+	if (stacksize(a) < 3 && (*a)->nu > (*a)->n->nu)
+		sa(a);
+	else if (stacksize(a) == 3)
+	{
+		if ((*a)->nu == numax(a))
+			ra(a);
+		else if ((*a)->n->nu == numax(a))
+			rra(a);
+		if ((*a)->nu > (*a)->n->nu)
+			sa(a);
+	}
+}
+
+void	init5(t_stack **a, t_stack **b)
 {
 	if (!a || !b)
 		return ;
-	while ((*a)->n->n->n != *a)
+	while (stacksize(a) > 3)
 		pb(a, b);
-	init2or3(a);
-/*	while (*b)
-	{
-		if ((*a)->p->nu > (*b)->nu)
-			rra(a);
-		if ((*a)->p->nu < (*b)->nu && (*a)->nu > (*b)->nu)
-			(void)pa(a, b), ra(a);
-	}
-	ra(a);*/
+	init3(a);
 }
 
 int	initpri(t_stack **a, t_stack **b)
@@ -91,9 +124,10 @@ int	initpri(t_stack **a, t_stack **b)
 	if (!a || !*a || !b)
 		return (1);
 	size = stacksize(a);
-	if (size > 0 && size < 4)
-		init2or3(a);
-	else if (size > 3 && size < 6)
-		initif4or5(a, b);
+//	if (size > 0 && size < 4)
+//		init3(a);
+//	else if (size > 3 && size < 6)
+	if (size < 6)
+		init5(a, b);
 	return (0);
 }
